3d65f60c1f426b7b37fc3e77227cdfea44c1ceeb
Tanner Hart <tannerhart24@yahoo.com>
2023-01-13 16:35:21 -0800

    Created bomb class to begin implementing them into the game

--- src/js/classes.js
+++ src/js/classes.js
@@ -209,3 +209,26 @@ class Particle {
     }
 }
 
+class Bomb {
+    constructor({ position, velocity, color = 'red' }) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 30;
+        this.color = color;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+        c.closePath()
+    }
+
+    update() {
+        this.draw();
+
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+}
