05aab9306c21eda3f77fef6566f55320e532b556
Tanner Hart <tannerhart24@yahoo.com>
2023-01-16 16:24:55 -0800

    Created a separate file to handle audio file using Howler

---
+++ src/js/audio.js
@@ -0,0 +1,27 @@
+const audio = {
+    backgroundMusic: new Howl({
+        src: './audio/backgroundMusic.wav'
+    }),
+    bonus: new Howl({
+        src: './audio/bonus.mp3',
+        volume: 0.8
+    }),
+    enemyShoot: new Howl({
+        src: './audio/enemyShoot.wav'
+    }),
+    explode: new Howl({
+        src: './audio/explode.wav'
+    }),
+    gameOver: new Howl({
+        src: './audio/gameOver.mp3'
+    }),
+    select: new Howl({
+        src: './audio/select.mp3'
+    }),
+    shoot: new Howl({
+        src: './audio/shoot.wav'
+    }),
+    start: new Howl({
+        src: './audio/start.mp3'
+    })
+}
